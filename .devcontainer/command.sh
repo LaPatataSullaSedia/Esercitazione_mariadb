210648310a87e608abcfed7948c7637bb0f48444
wheelchaired_potato <marro.simone@iisgalvanimi.edu.it>
2025-09-15 09:27:59 +0000

    primo

--- .devcontainer/command.sh
+++ .devcontainer/command.sh
@@ -1,7 +1,4 @@
-#sudo apt update
-
-#sudo apt install mariadb-server -y
-
-#sudo service mariadb start
-
+sudo apt update
+sudo apt install mariadb-server -y
+sudo service mariadb start
 sudo npm install -g @angular/cli
