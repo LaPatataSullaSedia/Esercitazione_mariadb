892609e3d8ae4fd426ff6a12130078ee7523154a
wheelchaired_potato <159021761+LaPatataSullaSedia@users.noreply.github.com>
2025-09-17 09:51:52 +0000

    provaa

--- .devcontainer/command.sh
+++ .devcontainer/command.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt install mariadb-server -y
 sudo service mariadb start
+pip install mysql
 sudo npm install -g @angular/cli
--- .devcontainer/main.py
+++ .devcontainer/main.py
@@ -6,8 +6,33 @@ mydb = mysql.connector.connect(
   password="nigga"
 )
 
+
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
+try:
+    mycursor.execute("CREATE USER IF NOT EXISTS 'fatass'@'localhost' IDENTIFIED BY 'nigga';")
+    mycursor.execute("GRANT ALL PRIVILEGES ON *.* TO 'fatass'@'localhost' WITH GRANT OPTION;")
+    mycursor.execute("FLUSH PRIVILEGES;")
+    print("Utente 'fatass' creato con successo e privilegi concessi.")
+except mysql.connector.Error as err:
+    print(f"Errore durante la creazione dell'utente: {err}")
 
+# Chiudi la connessione
+mycursor.close()
+mydb.close()
 
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="fatass",  # Ora l'utente fatass può connettersi
+        password="nigga"  # La password che hai impostato per l'utente 'fatass'
+    )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW DATABASES;")
+    print(mycursor.fetchall())
+
+    mycursor.close()
+    mydb.close()
+except mysql.connector.Error as err:
+    print(f"Errore durante la connessione come 'fatass': {err}")
